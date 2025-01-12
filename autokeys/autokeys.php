d76728205e426de1416af5e75c6db9256bc00a2b
Станислав Гольцов <irbees2008@gmail.com>
2025-01-12 17:34:09 +0500

    обновление

--- autokeys/autokeys.php
+++ autokeys/autokeys.php
@@ -1,49 +1,63 @@
 <?php
 // Protect against hack attempts
-if (!defined('NGCMS')) die ('HAL');
+if (!defined('NGCMS')) die('HAL');
+
 // Load library
 include_once(root . "/plugins/autokeys/lib/class.php");
 
 // News filtering class
-class autoKeysNewsFilter extends NewsFilter {
+class AutoKeysNewsFilter extends NewsFilter
+{
 
-	function addNews(&$tvars, &$SQL) {
-
-		if ($_POST['autokeys_generate'] == 1) {
-			$SQL['keywords'] = akeysGetKeys(array('content' => $SQL['content'], 'title' => $SQL['title']));
+	public function addNews(&$tvars, &$SQL)
+	{
+		if (!empty($_POST['autokeys_generate']) && $_POST['autokeys_generate'] == 1) {
+			$SQL['keywords'] = akeysGetKeys([
+				'content' => $SQL['content'] ?? '',
+				'title' => $SQL['title'] ?? ''
+			]);
 		}
 
 		return 1;
 	}
 
-	function editNews($newsID, $SQLold, &$SQLnew, &$tvars) {
-
-		if ($_POST['autokeys_generate'] == 1) {
-			$SQLnew['keywords'] = akeysGetKeys(array('content' => $SQLnew['content'], 'title' => $SQLnew['title']));
+	public function editNews($newsID, $SQLold, &$SQLnew, &$tvars)
+	{
+		if (!empty($_POST['autokeys_generate']) && $_POST['autokeys_generate'] == 1) {
+			$SQLnew['keywords'] = akeysGetKeys([
+				'content' => $SQLnew['content'] ?? '',
+				'title' => $SQLnew['title'] ?? ''
+			]);
 		}
 
 		return 1;
 	}
 
-	function editNewsForm($newsID, $SQLold, &$tvars) {
-
+	public function editNewsForm($newsID, $SQLold, &$tvars)
+	{
 		global $twig;
-		$tpath = locatePluginTemplates(array('editnews'), 'autokeys', pluginGetVariable('autokeys', 'localsource'));
+
+		$tpath = locatePluginTemplates(['editnews'], 'autokeys', pluginGetVariable('autokeys', 'localsource'));
 		$xt = $twig->loadTemplate($tpath['editnews'] . '/editnews.tpl');
-		$tvars['plugin']['autokeys'] = $xt->render(array('flags' => array('checked' => pluginGetVariable('autokeys', 'activate_edit'))));
+		$tvars['plugin']['autokeys'] = $xt->render([
+			'flags' => ['checked' => pluginGetVariable('autokeys', 'activate_edit')]
+		]);
 
 		return 1;
 	}
 
-	function addNewsForm(&$tvars) {
-
+	public function addNewsForm(&$tvars)
+	{
 		global $twig;
-		$tpath = locatePluginTemplates(array('addnews'), 'autokeys', pluginGetVariable('autokeys', 'localsource'));
+
+		$tpath = locatePluginTemplates(['addnews'], 'autokeys', pluginGetVariable('autokeys', 'localsource'));
 		$xt = $twig->loadTemplate($tpath['addnews'] . '/addnews.tpl');
-		$tvars['plugin']['autokeys'] = $xt->render(array('flags' => array('checked' => pluginGetVariable('autokeys', 'activate_add'))));
+		$tvars['plugin']['autokeys'] = $xt->render([
+			'flags' => ['checked' => pluginGetVariable('autokeys', 'activate_add')]
+		]);
 
 		return 1;
 	}
 }
 
-register_filter('news', 'autokeys', new autoKeysNewsFilter);
+register_filter('news', 'autokeys', new AutoKeysNewsFilter);
